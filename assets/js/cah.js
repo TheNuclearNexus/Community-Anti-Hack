de2f77f4a4c153abfd32d1ec70fbf19e1c4f3863
Nicole <gfpascoe@icloud.com>
2019-04-16 17:40:03 +0930

    Moved UIkit files, added custom CSS/JS

---
+++ assets/js/cah.js
@@ -0,0 +1,6 @@
+const themeToggle = document.getElementsByClassName("cah-theme");
+
+function themeChange() {
+  document.documentElement.classList.toggle("uk-light");
+  document.documentElement.classList.toggle("uk-background-secondary");
+}
