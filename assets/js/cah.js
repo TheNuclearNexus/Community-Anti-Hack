cd1e5c8a3f749b4323ecbed1df5cf2e14c3aac86
Nicole <gfpascoe@icloud.com>
2019-04-16 19:22:16 +0930

    Added theme switch animation, removed unused JS

--- assets/js/cah.js
+++ assets/js/cah.js
@@ -1,6 +1,8 @@
-const themeToggle = document.getElementsByClassName("cah-theme");
-
 function themeChange() {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 250);
   document.documentElement.classList.toggle("uk-light");
   document.documentElement.classList.toggle("uk-background-secondary");
 }
