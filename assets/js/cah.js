1af380e8c38165da0c15af730f51561883fb7f62
Nicole <gfpascoe@icloud.com>
2019-04-17 07:58:22 +0930

    Tweaked things
    Disabled light mode because I can't get the switch working the way I want it

--- assets/js/cah.js
+++ assets/js/cah.js
@@ -1,8 +1,24 @@
-function themeChange() {
-  document.documentElement.classList.add("transition");
-  window.setTimeout(() => {
-    document.documentElement.classList.remove("transition");
-  }, 250);
-  document.documentElement.classList.toggle("uk-light");
-  document.documentElement.classList.toggle("uk-background-secondary");
-}
+/*const theme = localStorage.getItem("theme");
+
+window.onload = () => {
+  const theme = localStorage.getItem("theme");
+  if (!theme) return setTheme("light");
+  return setTheme(theme);
+};
+
+const setTheme = color => {
+  setTheme(theme ? (theme === "dark" ? "dark" : "light") : "light");
+
+  if (color === "dark") {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.classList.add("uk-light");
+    document.documentElement.classList.add("uk-background-secondary");
+  } else {
+    localStorage.setItem("theme", "light");
+    document.documentElement.classList.remove("uk-light");
+    document.documentElement.classList.remove("uk-background-secondary");
+  }
+};
+
+setTheme(theme ? (theme === "dark" ? "dark" : "light") : "light");
+*/
